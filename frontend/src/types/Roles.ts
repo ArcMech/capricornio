6089fbc139040305da2f37c695b6daba5d3040c0
Arkadiusz Koziarz <arek.krz@gmail.com>
2022-02-10 21:45:07 +0100

    [feat] Add types to frontend

---
+++ frontend/src/types/Roles.ts
@@ -0,0 +1,1 @@
+export type Roles = 'Admin' | 'Manager' | 'Team' | 'Customer'
---
+++ frontend/src/types/User.ts
@@ -0,0 +1,11 @@
+import { Roles } from './Roles'
+
+export type User = {
+  id: number
+  name: string
+  first_name: string
+  last_name: string
+  email: string
+  role: Roles
+  access_token: string
+}
