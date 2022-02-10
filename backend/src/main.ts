ba1fbc94df1f6061fd6b1ca0feb0202e11e65b4e
Arkadiusz Koziarz <arek.krz@gmail.com>
2022-02-10 21:45:25 +0100

    [feat] Enable cors on backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true })
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
       whitelist: true,
       transform: true,
     }),
-  );
-  await app.listen(3000);
+  )
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
